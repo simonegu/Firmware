6136230613fc17a6d7a4cd247a30e5f1fba1d9be
Andreas Antener <antener_a@gmx.ch>
2015-09-24 15:57:51 +0200

    renamed heading controller to wheel controller, added groundspeed dependency and separate parameters

--- src/lib/ecl/module.mk
+++ src/lib/ecl/module.mk
@@ -39,7 +39,7 @@ SRCS		 = 	attitude_fw/ecl_controller.cpp \
 			attitude_fw/ecl_pitch_controller.cpp \
 			attitude_fw/ecl_roll_controller.cpp \
 			attitude_fw/ecl_yaw_controller.cpp \
-            attitude_fw/ecl_heading_controller.cpp \
+            attitude_fw/ecl_wheel_controller.cpp \
 			l1/ecl_l1_pos_controller.cpp
 
 MAXOPTIMIZATION	 = -Os
